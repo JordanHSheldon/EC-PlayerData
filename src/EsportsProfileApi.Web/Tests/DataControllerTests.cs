7940ea6efa75b36109660c838cad62ca652e1f67
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-11-28 10:41:09 -0500

    started writing tests to wrap around the methods create in orchestrators and controllers.

---
+++ src/EsportsProfileApi.Web/Tests/DataControllerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests;
+
+using EsportsProfileWebApi.CROSSCUTTING.Requests.Data;
+using EsportsProfileWebApi.CROSSCUTTING.Responses.Data;
+using EsportsProfileWebApi.DOMAIN;
+using EsportsProfileWebApi.DOMAIN.Orchestrators.Settings;
+using EsportsProfileWebApi.INFRASTRUCTURE;
+using EsportsProfileWebApi.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class DataControllerTests
+{
+    private readonly Mock<IDataOrchestrator> mockDataOrchestrator;
+    private readonly DataController _dataController;
+
+    public DataControllerTests()
+    {
+        mockDataOrchestrator = new Mock<IDataOrchestrator>();
+        _dataController = new DataController(mockDataOrchestrator.Object);
+    }
+
+}
---
+++ src/EsportsProfileApi.Web/Tests/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests;
+
+using EsportsProfileWebApi.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class UserControllerTests
+{
+    //private readonly Mock<IUserOrchestrator> mockUserOrchestrator;
+    private readonly UserController _userController;
+
+    //[SetUp]
+    public UserControllerTests()
+    {
+        //mockUserOrchestrator = new Mock<IUserOrchestrator>();
+        _userController = new UserController();
+    }
+
+    [Test]
+    public void LoginGiven_LoginRequestReturns()
+    {
+        ////Arrange
+        //var request = new GetDataRequest();
+        //var response = new GetDataResponse();
+        ////Act
+        //var result = _userController.;
+
+        ////Assert
+        //Assert.AreEqual(response, 1);
+    }
+
+}
