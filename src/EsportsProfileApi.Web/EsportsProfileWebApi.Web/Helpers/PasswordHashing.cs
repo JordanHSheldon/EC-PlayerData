76c29b787100d266fb1390f752418098236026f4
Jordan Sheldon <jordanhsheldon@gmail.com>
2024-01-06 15:38:20 -0500

    adjusted some unit tests and added passwordhashing helper to be used in login and registration.

---
+++ src/EsportsProfileApi.Web/EsportsProfileWebApi.Web/Helpers/PasswordHashing.cs
@@ -0,0 +1,25 @@
+﻿namespace EsportsProfileWebApi.Web.Helpers;
+
+using System.Security.Cryptography;
+using System.Text;
+
+public class PasswordHashing
+{
+    public static string HashPassword(string password)
+    {
+        if (String.IsNullOrEmpty(password))
+            return String.Empty;
+
+        using (var sha = new SHA256Managed())
+        {
+            byte[] textData = Encoding.UTF8.GetBytes(password);
+            byte[] hash = sha.ComputeHash(textData);
+            return BitConverter.ToString(hash).Replace("-", String.Empty);
+        }
+    }
+
+    public static bool VerifyPassword(string password, string hashedPassword)
+    {
+        return HashPassword(password) == hashedPassword;
+    }
+}
