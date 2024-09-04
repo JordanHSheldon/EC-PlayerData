f7b778ced983dd57bbcd481867360454c39a82ad
jordansheldon <Jordanhsheldon@gmail.com>
2024-09-04 11:10:22 -0400

    added line

--- src/EsportsProfileWebApi.Web/Controllers/DTOs/Data/PeripheralDTO.cs
+++ src/EsportsProfileWebApi.Web/Controllers/DTOs/Data/PeripheralDTO.cs
@@ -3,6 +3,7 @@ namespace EsportsProfileWebApi.Web.Controllers.DTOs.Data;
 public class PeripheralDTO
 {
     public string? Name { get; set; }
+    
     public string? Url { get; set; }
 
     public string? Type { get; set; }
